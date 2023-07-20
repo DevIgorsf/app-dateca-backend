1ae5763fdd323f4f80d36a0f7dc9a868791c7532
Igor Soares Fernandes <iigorsf.dev@gmail.com>
2023-07-20 16:50:02 -0300

    Ajuste do record ProfessorCreateDTO.

--- src/main/java/com/dat/dateca/domain/professor/ProfessorCreateDTO.java
+++ src/main/java/com/dat/dateca/domain/professor/ProfessorCreateDTO.java
@@ -1,12 +1,13 @@
 package com.dat.dateca.domain.professor;
 
 public record ProfessorCreateDTO(
+        Long id,
         Long registrationNumber,
         String name,
         String phone,
         String email) {
 
     public ProfessorCreateDTO(Professor professor) {
-        this(professor.getRegistrationNumber(), professor.getName(), professor.getEmail(), professor.getPhone());
+        this(professor.getId(), professor.getRegistrationNumber(), professor.getName(), professor.getPhone(), professor.getEmail());
     }
 }
